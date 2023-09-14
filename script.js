4d6ebfc481f3d202e45f9657d90ead4f751e2e55
Ranjit Sahu <ranjitsahu5558@gmail.com>
2023-09-14 22:57:22 +0530

    GSAP Animation

---
+++ script.js
@@ -0,0 +1,121 @@
+var tl = gsap.timeline();
+
+tl.from(".nav-bar h4",{
+    y:-50,
+    duration:0.8,
+    delay:0.3,
+    stagger:0.3,
+    opacity:0
+})
+
+.from(".circle",{
+    scale:0,
+    rotatex:360,
+    duration:0.8,
+    opacity:0
+})
+
+.from(".large-cap",{
+    opacity:0,
+    duration:1.4
+})
+
+.from(".lower-right",{
+    scale:0,
+    rotatex:360,
+    duration:0.8,
+    opacity:0
+})
+
+.from(".rectangle",{
+    scale:0,
+    rotate:360,
+    duration:0.8,
+    opacity:0
+})
+
+.from(".macro-circle",{
+    scale:0,
+    rotatex:360,
+    duration:0.8,
+    opacity:0
+})
+
+.from(".icons-right img",{
+    y:-50,
+    duration:1.3,
+    delay:0.3,
+    stagger:0.6,
+    opacity:0
+})
+
+.from(".right-line",{
+    y:-50,
+    duration:1.3,
+    delay:0.3,
+    stagger:0.6,
+    opacity:0
+
+})
+
+.from(".music",{
+    y:-50,
+    duration:1.3,
+    delay:0.3,
+    stagger:0.6,
+    opacity:0
+})
+
+.from(".music-lover",{
+    y:-50,
+    duration:1.3,
+    delay:0.3,
+    stagger:0.6,
+    opacity:0
+})
+
+.from(".arrows button",{
+    y:-50,
+    duration:1.3,
+    delay:0.3,
+    stagger:0.6,
+    opacity:0
+})
+
+.from(".date",{
+    y:-50,
+    duration:1.3,
+    delay:0.3,
+    stagger:0.6,
+    opacity:0
+})
+
+
+.from(".left-mlline",{
+    y:-50,
+    duration:1.3,
+    delay:0.3,
+    stagger:0.6,
+    opacity:0
+})
+.from(".music-library",{
+    rotate:360,
+    duration:1.3,
+    delay:0.3,
+    stagger:0.6,
+    opacity:0
+})
+.from(".text ",{
+    scale:0,
+    y:-50,
+    duration:0.9,
+    delay:0.3,
+    stagger:0.6,
+    opacity:0
+})
+
+.from(".a-man",{
+    opacity:0,
+    y:200,
+    duration:0.4
+})
